0deddbb001110dcfae559c3d89c4ce6adcd28421
David Mordechai <david.mordechai79@gmail.com>
2022-10-21 13:22:28 +0300

    Add project files.

---
+++ BenchmarkDemo.Console/GuiderBenchmarks.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDemo.Library;
+using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkDemo.Console;
+
+[MemoryDiagnoser(displayGenColumns:false)]
+public class GuiderBenchmarks
+{
+    private static readonly Guid TestIdAsGuid = Guid.Parse("acd9fd16-d24e-48c3-b0cc-fc63c545bb86");
+    private const string TestIdAsString = "Fv3ZrE7Sw0iwzPxjxUW7hg";
+
+    [Benchmark]
+    public Guid ToGuidFromString()
+    {
+        return Guider.ToGuidFromString(TestIdAsString);
+    }
+
+
+    [Benchmark]
+    public Guid ToGuidFromStringOptimize()
+    {
+        return Guider.ToGuidFromStringOptimize(TestIdAsString);
+    }
+
+    [Benchmark]
+    public string ToStringFromGuid()
+    {
+        return Guider.ToStringFromGuid(TestIdAsGuid);
+    }
+    
+    [Benchmark]
+    public string ToStringFromGuidOptimize()
+    {
+        return Guider.ToStringFromGuidOptimize(TestIdAsGuid);
+    }
+}
---
+++ BenchmarkDemo.Console/Program.cs
@@ -0,0 +1,33 @@
+﻿using BenchmarkDemo.Console;
+using BenchmarkDemo.Library;
+using BenchmarkDotNet.Running;
+
+var isDebugMode = false;
+#if DEBUG
+    isDebugMode = true;
+#endif
+
+if (isDebugMode)
+{
+    var id = Guid.NewGuid();
+    Console.WriteLine(id);
+
+    var base64Id = Convert.ToBase64String(id.ToByteArray());
+    Console.WriteLine(base64Id);
+
+    var urlFriendlyBase64IdOptimize = Guider.ToStringFromGuidOptimize(id);
+    Console.WriteLine(urlFriendlyBase64IdOptimize);
+
+    var urlFriendlyBase64Id = Guider.ToStringFromGuid(id);
+    Console.WriteLine(urlFriendlyBase64Id);
+
+    var idAgain = Guider.ToGuidFromString(urlFriendlyBase64Id);
+    Console.WriteLine(idAgain);
+
+    var idAgainOptimize = Guider.ToGuidFromStringOptimize(urlFriendlyBase64Id);
+    Console.WriteLine(idAgainOptimize);
+
+    return;
+}
+
+BenchmarkRunner.Run<GuiderBenchmarks>();
